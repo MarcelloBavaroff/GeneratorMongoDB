41e1e6a81258b26adc03041173725a08a8e2ea9a
Francesco Puddu <10583315@polimi.it>
2021-12-11 23:15:56 +0100

    Finito il componente che crea la persona e genera il qr

--- qr_generator.py
+++ qr_generator.py
@@ -1,14 +1,37 @@
 import qrcode
-from PIL import Image
 import sys
+import json
+from pymongo import MongoClient
 
 def create_qr(code):
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
     qr.add_data(code)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-    img.save("qr_"+code+".png")
+    img.save("qr.png")
+
+def create_person(data, connection):
+    collection = connection["Certificate"]
+
+    # do not enforce the schema here, more flexibility
+    cert =  {
+            "PERSON": data,
+            "VACCINATION": [],
+            "TEST": []
+            }
+
+    id = collection.insert_one(cert)
+    return id.inserted_id
 
 if __name__ == "__main__":
+
     inp = sys.argv[1]
-    create_qr(inp)
+    f = open(inp, "r")
+    f = json.load(f)
+
+    client = MongoClient("localhost", 27017)
+
+    id = create_person(f, client["Coviddi"])
+    print(id)
+
+    create_qr(str(id))
